0281e06d492b2108192850eae4d017d57a0a0442
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-08 12:57:12 +1200

    Exercise's work

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -12,10 +12,22 @@ public class BarkingDog {
      * We have to wake up if the dog is barking before 8 or after 22 hours so that in that case return true
      * In all other cases return false
      *
-     * If the hourOfDat parameter is less than 0 or greater than 23 return false
+     * If the hourOfDay parameter is less than 0 or greater than 23 return false
      *
      */
 
 
+    // given that hourOfDay is 24hr
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        if(barking && (hourOfDay < 8 && hourOfDay >= 0)) {
+            return true;
+        } else if(barking && (hourOfDay > 22 && hourOfDay <= 23)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
 }
